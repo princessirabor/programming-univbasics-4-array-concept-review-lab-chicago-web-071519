50b28b4d5fecc9baeb95b2abc9681c3595e0c8ec
Princess Irabor <irabor.princess1@gmail.com>
2019-06-25 23:02:22 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -22,4 +22,11 @@ end
 
 def find_min_value(array)
   # Add your solution here
+  min = array[0]
+  array.length.times do |index|
+    if array[index] < min
+      min = array[index]
+    end
+  end
+  min
 end
